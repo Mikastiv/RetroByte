819ffff0e9609807d6b9d19747219edd6db60d22
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-11-12 16:46:12 -0500

    Refactored main, extracted setdrawcolor and drawcolor + escape event. Created Gameboy.zig and Cpu.zig

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 const c = @import("c.zig");
+const builtin = @import("builtin");
 
 const SDLError = error{
     SDLInitFailed,
     SDLWindowCreationFailed,
     SDLRendererCreationFailed,
+    SDLSetDrawColorFailed,
+    SDLClearFramebufferFailed,
 };
 
 const SDLContext = struct {
@@ -51,42 +54,54 @@ const SDLContext = struct {
         c.SDL_DestroyWindow(self.window);
         c.SDL_Quit();
     }
+
+    fn setDrawColor(self: Self) SDLError!void {
+        if (c.SDL_SetRenderDrawColor(self.renderer, 0, 0, 0, c.SDL_ALPHA_OPAQUE) < 0) {
+            std.log.err("failed to set draw color: {s}", .{c.SDL_GetError()});
+            return error.SDLSetDrawColorFailed;
+        }
+    }
+
+    fn clearFramebuffer(self: Self) SDLError!void {
+        if (c.SDL_RenderClear(self.renderer) < 0) {
+            std.log.err("failed to clear framebuffer: {s}", .{c.SDL_GetError()});
+            return error.SDLClearFramebufferFailed;
+        }
+    }
 };
+
+fn getRom(allocator: std.mem.Allocator) ![]const u8 {
+    const stderr = std.io.getStdErr().writer();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+    if (args.len != 2) {
+        const basename = std.fs.path.basename(args[0]);
+        try stderr.print("usage: {s} <ROM file>", .{basename});
+        return error.NoArgGiven;
+    }
+    std.log.info("cartridge: {s} \n", .{args[1]});
+    return &.{};
+}
 
 pub fn main() !u8 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-
     const allocator = arena.allocator();
 
-    const stderr = std.io.getStdErr().writer();
-
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    if (args.len != 2) {
-        const basename = std.fs.path.basename(args[0]);
-        try stderr.print("usage: {s} <ROM file>\n", .{basename});
-        return 1;
-    }
-
-    std.log.info("cartridge: {s} \n", .{args[1]});
-
+    const rom = getRom(allocator);
+    _ = rom;
     const sdl = try SDLContext.init("RetroByte", 800, 600);
-    // defer sdl.deinit();
-
-    if (c.SDL_SetRenderDrawColor(sdl.renderer, 0, 0, 0, c.SDL_ALPHA_OPAQUE) < 0) {
-        std.log.err("failed to set draw color: {s}", .{c.SDL_GetError()});
-        return 1;
+    defer {
+        if (builtin.os.tag != .windows) sdl.deinit(); // No deinit on fucking windows because it's too slow. Later looser.
     }
+    try sdl.setDrawColor();
+    try run(sdl);
+}
 
+fn run(sdl: SDLContext) !void {
     var running = true;
     while (running) {
-        if (c.SDL_RenderClear(sdl.renderer) < 0) {
-            std.log.err("failed to clear framebuffer: {s}", .{c.SDL_GetError()});
-            return 1;
-        }
-
+        try sdl.clearFramebuffer();
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.type) {
@@ -94,10 +109,18 @@ pub fn main() !u8 {
                     running = false;
                     break;
                 },
+                c.SDL_KEYDOWN => {
+                    switch (event.key.keysym.sym) {
+                        c.SDLK_ESCAPE => {
+                            running = false;
+                            break;
+                        },
+                        else => {},
+                    }
+                },
                 else => {},
             }
         }
     }
-
     return 0;
 }
