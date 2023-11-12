40d398e6f4d8e6089e8c1adbe0c960d492481937
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-11-12 15:59:14 -0500

    Before pull

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
+
     const args = try std.process.argsAlloc(allocator);
     if (args.len == 2) std.debug.print("Cartridge: {s} \n", .{args[1]});
 }
